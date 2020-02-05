e212b15450f4662b9c85f035672332da45d66caf
Heather Nuffer <zzzuzz@gmail.com>
2020-02-05 14:03:59 -0700

    Added instructions in README.md for running the app locally

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Context API + State Storage
+
+## Deployed Site:
+https://context-api-with-storage-options.surge.sh/
+
+## Overview: 
+
 Quick example of using context API for passing state to a child component.
 
 Also, has 3 different ways of storing the state:
---
+++ build/precache-manifest.22ccd25aa4685aeef6519e4ec824e1c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f3f4c6c887b6e584c918a76c15ccc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92ceaaa3a07056a21f19",
+    "url": "/static/css/main.3b61fedc.chunk.css"
+  },
+  {
+    "revision": "87d67275e1e31a911ad5",
+    "url": "/static/js/2.663d3ca6.chunk.js"
+  },
+  {
+    "revision": "92ceaaa3a07056a21f19",
+    "url": "/static/js/main.e41edd15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
